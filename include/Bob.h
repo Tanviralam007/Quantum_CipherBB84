b73b53c2cfeaf4df6e7ba830fd250fb1d86394d2
Tanviralam007 <tanviralam451@gmail.com>
2025-03-10 01:32:54 +0600

    Bob: Implement key generation logic updated

--- include/Bob.h
+++ include/Bob.h
@@ -18,6 +18,8 @@ class Bob {
         void generate_measurement_basis(int size);
         void measure_qbits();
         void compare_results(const std::vector<int>& alice_basis_sequence);
+
+        // getters
         std::vector<int>& get_measured_bits();
         std::vector<int>& get_measurement_basis();
 };
