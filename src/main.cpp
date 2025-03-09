4fafcb2967e9f31fc51c1a3b8747e75de9d99b6a
Tanviralam007 <tanviralam451@gmail.com>
2025-03-10 01:51:36 +0600

    Driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
-#include <iostream>
+#include "../include/BB84Protocol.h"
+#include <iostream> 
 
 int main(int argc, char **argv){
+    BB84Protocol protocol = BB84Protocol();
+    protocol.run_protocol(10);
+    protocol.outcome();
+
     
+    return 0;
 }
